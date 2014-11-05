1222493096e0165c238330e5aa0140e04ef5289b
HughesDaniel <danielh@toxicshock.biz>
2014-11-05 12:09:39 -0700

    added foundation for answer bloquery dialog

--- bloquery/src/main/java/com/bloc/bloquery/fragments/AnswersFragment.java
+++ bloquery/src/main/java/com/bloc/bloquery/fragments/AnswersFragment.java
@@ -3,11 +3,15 @@ package com.bloc.bloquery.fragments;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bloc.bloquery.R;
 import com.bloc.bloquery.adapters.AnswersAdapter;
@@ -52,6 +56,8 @@ public class AnswersFragment extends Fragment {
 
         mQuestionId = getArguments().getString(KEY_QUESTION_ID);
         mQuestion = getArguments().getString(KEY_QUESTION);
+
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -78,5 +84,21 @@ public class AnswersFragment extends Fragment {
         mListView.setAdapter(mAdapter);
 
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.answers_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        int id = item.getItemId();
+        if (id == R.id.answer_bloquery) {
+            Toast.makeText(getActivity(), "coming soon!", Toast.LENGTH_LONG).show();
+        }
+        return false;
     }
 }
