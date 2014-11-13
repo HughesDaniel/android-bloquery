cb725a0909ce8203fb27b3c477b64fce84402edb
HughesDaniel <danielh@toxicshock.biz>
2014-11-12 21:03:59 -0700

    implemented interest indicator image view for questions

--- bloquery/src/main/java/com/bloc/bloquery/adapters/QuestionsAdapter.java
+++ bloquery/src/main/java/com/bloc/bloquery/adapters/QuestionsAdapter.java
@@ -1,9 +1,12 @@
 package com.bloc.bloquery.adapters;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bloc.bloquery.R;
@@ -19,6 +22,8 @@ public class QuestionsAdapter extends ParseQueryAdapter {
     private static final String PARSE_NUM_ANSWERS = "numberOfAnswers";
 
     private Context mContext;
+    private int mNumberAnswers;
+    private ImageView mInterestIndicator;
 
     public QuestionsAdapter(Context context, QueryFactory queryFactory) {
         super(context, queryFactory);
@@ -29,6 +34,8 @@ public class QuestionsAdapter extends ParseQueryAdapter {
     public View getItemView(ParseObject object, View v, ViewGroup parent) {
         super.getItemView(object, v, parent);
 
+        mNumberAnswers = object.getInt(PARSE_NUM_ANSWERS);
+
         View cView = v;
 
         if (cView == null) {
@@ -36,17 +43,44 @@ public class QuestionsAdapter extends ParseQueryAdapter {
             cView = inflater.inflate(R.layout.question_in_lv, parent, false);
         }
 
-
+        // Text View that displays question
         TextView questionTextView = (TextView) cView.findViewById(R.id.tv_question_body);
         questionTextView.setText(object.getString(PARSE_QUESTION));
 
+        // Displays the number of answers
         TextView numberOfAnswersTextView = (TextView) cView.findViewById(R.id.tv_number_answers);
-        numberOfAnswersTextView.setText(String.valueOf(object.getInt(PARSE_NUM_ANSWERS)) + " "
+        numberOfAnswersTextView.setText(mNumberAnswers + " "
                             + mContext.getString(R.string.answers));
+
+        mInterestIndicator = (ImageView) cView.findViewById(R.id.iv_interest_indicator);
+        setInterestIndicator();
+
 
 
         return cView;
 
+    }
+
+    // Sets the image to display based upon the number of answers to the question
+    private void setInterestIndicator() {
+        int currentVersion = Build.VERSION.SDK_INT;
+        if (currentVersion >= 16 && mNumberAnswers > 2) {
+            mInterestIndicator.setBackground(getBolts());
+        } else if (currentVersion == 15 && mNumberAnswers > 2) {
+            mInterestIndicator.setBackgroundDrawable(getBolts());
+        }
+    }
+
+    // returns the correct graphic (lightning bolts) to display in indicator
+    // only called if there are at least 3 answers
+    private Drawable getBolts() {
+        if (mNumberAnswers > 9) {
+            return mContext.getResources().getDrawable(R.drawable.triple_bolt);
+        } else if (mNumberAnswers > 5) {
+            return mContext.getResources().getDrawable(R.drawable.double_bolt);
+        } else {
+            return mContext.getResources().getDrawable(R.drawable.single_bolt);
+        }
     }
 
 }
